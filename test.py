9e2242895a9d31927c0a61193effdbd62248eed7
Pietro Santin <pietro.santin@yahoo.com>
2022-04-02 22:02:33 +0200

    test 2

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
-x = 10 
+x = 20 
 y = 20
 #addded a comment
-if (x >= y):
-    print("x is greater or equal than y")
+if (x > y):
+    print("x is greater than y")
+elif (x == y):
+    print("x equal to y")
 else: 
     print("y is greater than x")
